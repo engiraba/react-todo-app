c1dede0526ce05b051bda2236b11707514689d5b
Eric <ngirabacu.eric@gmail.com>
2021-11-24 13:56:13 +0100

    Created custom component for the item in the todo list

---
+++ src/ItemToDo.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+
+function ItemToDo(props) {
+    const [done, setCheck] = React.useState(false);
+
+    return (
+       
+
+        <ListItem disablePadding > 
+
+            <ListItemButton >
+            <Checkbox
+                    checked={done}
+                    onChange={
+                        (event) => setCheck(event.target.checked)
+                    }
+                />
+                <ListItemText
+                    primary={props.text}
+                />
+
+            </ListItemButton >
+
+            
+        </ListItem>
+
+    );
+
+} export default ItemToDo;
