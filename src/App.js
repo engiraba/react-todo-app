197ad612523d4310e2bd524cf25f951b2dfe637b
Eric <ngirabacu.eric@gmail.com>
2021-11-24 10:48:08 +0100

    Added list and list items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import Checkbox from '@mui/material/Checkbox';
 
 function App() {
+  const [dense, setDense] = React.useState(false);
 
-  const dummyData : [] = [
+  const dummyData = [
     'Do groceries shopping',
     'Repair micro-waves'
   ]
@@ -11,6 +19,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Box sx={{ width: '100%', maxWidth: 360, color: 'black', bgcolor: 'beige', borderRadius: '3%' }}>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+            <Checkbox
+              checked={dense}
+              onChange={(event) => setDense(event.target.checked)}
+            />
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+
         {
           dummyData.map((item, i) => {
             return <ul> {item} </ul>;
