4aa7ef41c4e031a97219910c08cf35e4d1751f60
Eric <ngirabacu.eric@gmail.com>
2021-11-24 13:56:40 +0100

    Integrated custom component on the main page app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
 import './App.css';
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import ItemToDo from './ItemToDo';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import Checkbox from '@mui/material/Checkbox';
 
 function App() {
-  const [dense, setDense] = React.useState(false);
 
   const dummyData = [
     'Do groceries shopping',
-    'Repair micro-waves'
+    'Repair micro-wave',
+    'Do laundry',
+    'Walk in the park',
+    'Clean up desk'
   ]
 
   return (
     <div className="App">
       <header className="App-header">
-      <Box sx={{ width: '100%', maxWidth: 360, color: 'black', bgcolor: 'beige', borderRadius: '3%' }}>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-        {
-          dummyData.map((item, i) => {
-            return (
-            
-            
-              <ListItem disablePadding>
-            <ListItemButton>
-            <Checkbox
-              checked={dense}
-              onChange={(event) => setDense(event.target.checked)}
-            />
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-            
-            );
-          })
-        }
-          
-         
-        </List>
-      </nav>
-    </Box>
+        <Box sx={{ width: '100%', maxWidth: 360, color: 'black', bgcolor: 'beige', borderRadius: '3%' }}>
+          <Divider />
+          <nav aria-label="secondary mailbox folders">
+            <List>
+              {
+                dummyData.map((item, i) => {
+                  return (
 
-        
+
+                   <div> <ItemToDo text={item}/>
+                    
+</div>
+                  );
+                })
+              }
+
+
+            </List>
+          </nav>
+        </Box>
+      
+
       </header>
+      
     </div>
   );
 }
